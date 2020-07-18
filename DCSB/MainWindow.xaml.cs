295b0b5cfa98beb9db826643e3f0bbe3760cfd74
Patrick Ramser <pramser@gmail.com>
2020-07-17 21:42:17 -0400

    Remove tray icon logic in startup

--- DCSB/MainWindow.xaml.cs
+++ DCSB/MainWindow.xaml.cs
@@ -10,8 +10,6 @@ namespace DCSB
 {
     public partial class MainWindow : Window
     {
-        NotifyIcon notifyIcon;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -19,25 +17,6 @@ namespace DCSB
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             DataContext = new ViewModel();
-
-            Icon icon;
-            using (Stream stream = System.Windows.Application.GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream)
-            {
-                icon = new Icon(stream);
-            }
-
-            MenuItem open = new MenuItem("Open", (sender, e) => Open());
-            MenuItem exit = new MenuItem("Exit", (sender, e) => Close());
-
-            notifyIcon = new NotifyIcon
-            {
-                Icon = icon,
-                Text = "Deathcounter and Soundboard",
-                Visible = true,
-                ContextMenu = new ContextMenu(new MenuItem[] { open, exit })
-            };
-
-            notifyIcon.Click += (sender, args) => Open();
         }
 
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -53,27 +32,12 @@ namespace DCSB
             base.OnStateChanged(e);
         }
 
-        protected override void OnClosed(EventArgs e)
-        {
-            if (notifyIcon != null)
-                notifyIcon.Dispose();
-
-            base.OnClosed(e);
-        }
-
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             if (DataContext is ViewModel viewModel)
             {
                 viewModel.WindowHandle = new WindowInteropHelper(this).Handle;
             }
-        }
-
-        private void Open()
-        {
-            Show();
-            WindowState = WindowState.Normal;
-            Activate();
         }
     }
 }
